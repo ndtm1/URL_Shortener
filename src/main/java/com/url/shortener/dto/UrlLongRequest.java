5155ffe04d7ff14493243d4f2deeee8a090f2137
neydtm <degtyarevn5@gmail.com>
2023-06-26 01:50:53 +0400

    refactor: setter's and getter's, change types of variables

--- src/main/java/com/url/shortener/dto/UrlLongRequest.java
+++ src/main/java/com/url/shortener/dto/UrlLongRequest.java
@@ -1,8 +1,28 @@
 package com.url.shortener.dto;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 public class UrlLongRequest {
     private int id;
-    private LocalDate expiresDate;
+
+    private String longUrl;
+
+    private Date expiresDate;
+
+    public String getLongUrl() {
+        return longUrl;
+    }
+
+    public void setLongUrl(String longUrl) {
+        this.longUrl = longUrl;
+    }
+
+    public Date getExpiresDate() {
+        return expiresDate;
+    }
+
+    public void setExpiresDate(Date expiresDate) {
+        this.expiresDate = expiresDate;
+    }
 }
