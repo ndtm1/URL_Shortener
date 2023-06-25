de2e925558a5602b47645c4997679a0032651d3f
neydtm <degtyarevn5@gmail.com>
2023-06-25 04:37:50 +0400

    feat: interacts with DB, converting base, url etc

---
+++ src/main/java/com/url/shortener/service/UrlService.java
@@ -0,0 +1,14 @@
+package com.url.shortener.service;
+
+import com.url.shortener.dto.UrlLongRequest;
+
+public class UrlService {
+
+    public String convertToShort(UrlLongRequest longUrl) {
+        return "";
+    }
+
+    public String getLongUrl(String shortUrl) {
+        return "";
+    }
+}
